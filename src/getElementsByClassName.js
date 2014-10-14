69c6900b27377f314b07a16f9c36d5775c4d6477
Ervin <ervinchow@gmail.com>
2014-10-13 18:56:34 -0700

    getElementsByClassName spec passed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,22 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var result = [];
+
+  function lookup(node) {
+
+    var children = node.children;
+    var arr = node.className === undefined ? [] : node.className.split(' ');
+    
+    _.each(arr, function(element){
+      if(element === className) {result.push(node);}
+  	});
+
+  	if(children.length){
+  		_.each(children, function(element){lookup(element);})
+  	}
+  }
+
+  lookup(document);
+  return result;
 };
